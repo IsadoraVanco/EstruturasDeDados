134606d3a61df15bfef72b85f709aa51de579802
isadora <isadoravanco@gmail.com>
2023-09-07 15:58:32 -0300

    feat: modificando cabeçalhos do exercicio de TAD

--- exercicios/3semestre/TAD/numeroComplexo/main.c
+++ exercicios/3semestre/TAD/numeroComplexo/main.c
@@ -1,16 +1,3 @@
-/*
-Créditos ao profº Gilberto, autor do exercício.
-
-1. Implemente um TAD “Número Complexo” e implemente as seguintes operações:
-    * Inicializa: atribui valores para os campos
-    * Imprime: imprime o número da forma “R + Ci”
-    * Copia: Copia o valor de um número para outro
-    * Soma: Soma dois números complexos
-    * EhReal: testa se um número é real
-
-    Por fim, faça um pequeno programa para testar o seu TAD. Lembre-se que em números complexos, 
-    cada número possui os campos real e imaginário
-*/
 #include <stdio.h>
 #include "numeroComplexo.h"
 
--- exercicios/3semestre/TAD/numeroComplexo/numeroComplexo.h
+++ exercicios/3semestre/TAD/numeroComplexo/numeroComplexo.h
@@ -1,5 +1,18 @@
-#ifndef __NUMERO_COMPLEXO_
-#define __NUMERO_COMPLEXO_
+/*
+Créditos ao profº Gilberto, autor do exercício.
+
+1. Implemente um TAD “Número Complexo” e implemente as seguintes operações:
+    * Inicializa: atribui valores para os campos
+    * Imprime: imprime o número da forma “R + Ci”
+    * Copia: Copia o valor de um número para outro
+    * Soma: Soma dois números complexos
+    * EhReal: testa se um número é real
+
+    Por fim, faça um pequeno programa para testar o seu TAD. Lembre-se que em números complexos, 
+    cada número possui os campos real e imaginário
+*/
+#ifndef _NUMERO_COMPLEXO_H
+#define _NUMERO_COMPLEXO_H
 
 typedef struct{
     double real; 
