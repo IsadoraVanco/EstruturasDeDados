ca603a829da2ec8f9c953e91a4ca9d5ad5171588
isadora <isadoravanco@gmail.com>
2023-09-15 21:30:41 -0300

    feat: Melhora no FilaDeProcessos

--- Exercicios/3semestre/VerificaParenteses/main.c
+++ Exercicios/3semestre/VerificaParenteses/main.c
@@ -19,14 +19,15 @@ em C, o último caractere da string é \0.
 #include "pilha.h"
 
 /*
-A função recebe uma expressão aritmética e verifica se os colchetes e 
-parênteses foram abertos e fechados corretamente.
+A função recebe uma expressão aritmética e verifica se os colchetes, 
+chaves e parênteses foram abertos e fechados corretamente.
 Retorna 1 caso tenha erro, senão, retorna 0.
 */
 int verificaExpressao(char *expressao){
-    int ultimo; //caso exista erro, representará a ultima chave aberta
+    int ultimo; //caso exista erro, representará a chave com possível erro
     int erro = 0; //aponta erro :)
     char *letra = expressao; //ponteiro para um caractere da expressão
+    
     Stack *pilha = newStack(); //cria uma nova pilha
 
     for(int i = 0; i < strlen(expressao); i++){
@@ -44,9 +45,9 @@ int verificaExpressao(char *expressao){
             ultimo = i;
 
         }else if(*letra == ')' || *letra == ']' || *letra == '}'){ //se o caractere for uma das chaves fechando
-            if(pop(pilha) == 0){ //chaves fechando a mais
-                ultimo = i;
-                i = strlen(expressao);
+            if(pop(pilha) == 0 || pop(pilha) != *letra){ //chaves fechando a mais OU ordem está errada
+                ultimo = i;                             
+                i = strlen(expressao); //para o laço
                 erro = 1;
             }
         }
@@ -54,16 +55,16 @@ int verificaExpressao(char *expressao){
         letra++;
     }
     
-    printf("%s\n", expressao);
+    printf("\n%s\n", expressao);
     
     if(isEmptyStack(pilha) && !erro){
-        printf("A expressão está correta.\n");
-    }else{ //faltou chaves para fechar
+        printf("A expressão está correta\n");
+    }else{ //faltou ou sobrou chaves para fechar
         for(int i = 0; i < ultimo; i++){
             printf(" ");
         }
         printf("^\n");
-        printf("Existe um erro na expressão.\n");
+        printf("Existe um erro na expressão\n");
 
     }
 
